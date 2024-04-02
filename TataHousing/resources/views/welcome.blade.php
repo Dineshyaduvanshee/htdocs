1ccd912c616f2444f59909f818f6f1dae70cd852
Dinesh yadav <dineshyadav74950@gmail.com>
2024-04-02 20:41:10 +0530

    In this project i have applied some routing changes

--- TataHousing/resources/views/welcome.blade.php
+++ TataHousing/resources/views/welcome.blade.php
@@ -1,3 +1,46 @@
-<div>
-<h1 style="background-color: cornflowerblue;text-align:c"></h1>
-</div>
+{{-- <div>
+<h1 style="background-color: cornflowerblue;text-align:c">!!This is Welcome Page!!</h1>
+</div> --}}
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Our Website</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f0f0f0;
+        }
+        .container {
+            width: 80%;
+            margin: 0 auto;
+            text-align: center;
+            padding: 50px 0;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            color: #666;
+            font-size: 18px;
+            line-height: 1.6;
+        }
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Our Website</h1>
+        <p>This is a simple welcome page template. Feel free to customize it according to your needs.</p>
+        <p>For more information, visit our <a href="#">About Us</a> page.</p>
+    </div>
+</body>
+</html>
--- TataHousing/routes/web.php
+++ TataHousing/routes/web.php
@@ -7,3 +7,74 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('/post/{id?}/comment/{commentid?}', function (string $id=null,string $commentid=null) {
+//     if($id){
+//         return "<h1>Psot Id :". $id ."</h1><h2>".$commentid."</h2>";
+//     }else{
+//         return "<h1>Psot Id not found</h1>";
+//     }
+
+// });
+
+// Route::get('/post/{id}', function (string $id) {
+//     if($id){
+//         return "<h1>Psot Id :". $id ."</h1>";
+//     }else{
+//         return "<h1>Psot Id not found</h1>";
+//     }
+
+// })->whereNumber('id');  //whereAlpha
+
+// Route::get('/post/{id}', function(string $id){
+//     if($id){
+//         return "<h1>Psot Id :". $id ."</h1>";
+//     }
+//     else{
+//         return "<h1>Psot Id not found</h1>";
+//     }
+// })->whereAlpha('id');
+
+// Route::get('/post/{id}', function(string $id){
+//     if($id){
+//         return "<h1>Psot Id :". $id ."</h1>";
+//     }else{
+//         return "<h1>Psot Id not found</h1>";
+//     }
+// })->whereAlphaNumeric('id');
+
+// Route::get('post/{id}', function(string $id){
+//     if($id){
+//         return "<h1>Psot Id :". $id ."</h1>";
+//     }else{
+//         return "<h1>Psot Id not found</h1>";
+//     }
+// })->whereIn('id',['movie','song','painting']);
+
+
+
+
+// Route::view('/post','post');
+
+// Route::get('/post', function () {
+//     return view("post");
+// });
+
+
+// Route::get('post/{id}', function(string $id){
+//     if($id){
+//         return "<h1>Psot Id :". $id ."</h1>";
+//     }else{
+//         return "<h1>Psot Id not found</h1>";
+//     }
+// })->where('id','[a-zA-Z]+');
+
+
+Route::get('post/{id}/comment/{commentid}', function(string $id,string $comment){
+    if($id){
+        return "<h1>Psot Id :". $id ." & Comment : ".$comment."</h1>";
+    }else{
+        return "<h1>Psot Id not found</h1>";
+    }
+})->where('id','[0-9]+')->whereAlpha('commentid');
